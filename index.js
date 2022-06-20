6078ccb3c0b17ed542bfb95db8c2b371a679d4d3
unknown <martin_suarez_mendez@outlook.com>
2022-06-20 12:19:14 -0500

    try/catch

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 console.log("im here");
 
-let one = document.getElementById("myElement");
+let toPrint = document.getElementById("myElement");
 
-fetch("http://localhost:5000/users/")
-  .then((res) => res.json())
-  .then((res) => {
-    console.log(res);
-    console.log(one);
-    one.innerHTML = res.map((val, ind, arr) => {
+// fetch("http://localhost:5000/users/")
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.log(res);
+//     console.log(one);
+//     one.innerHTML = res.map((val, ind, arr) => {
+//       const { id, name, place } = val;
+//       return `
+//         <ul>
+//             <li>ID: ${id}</li>
+//             <li>NAME: ${name}</li>
+//             <li>PLACE: ${place}</li>
+//         </ul>
+//       `;
+//     });
+//   });
+
+const myFetchedFunction = async () => {
+  try {
+    let var1 = await fetch("http://localhost:5000/users");
+    let var2 = await var1.json();
+    toPrint.innerHTML = var2.map((val, ind, arr) => {
       const { id, name, place } = val;
       return `
         <ul>
             <li>ID: ${id}</li>
             <li>NAME: ${name}</li>
-            <li>PLACE: ${place}</li>
-        </ul>
-      `;
+           <li>PLACE: ${place}</li>
+         </ul>
+        `;
     });
-  });
+  } catch (e) {
+    console.log(e);
+  }
+};
+myFetchedFunction();
